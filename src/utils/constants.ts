4748c840bbacd5760c796be04b201d06da6acdf2
Ibrahim Taofeek <taofeekibrahimope@gmail.com>
2025-07-20 13:18:51 +0000

    update fee to their normal

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,11 @@
-export const HANDLING_FEE_GBP = 0.5 // 25
-export const MINIMUM_ORDER_GBP = 1 // 100;
+export const HANDLING_FEE_GBP = 25
+export const MINIMUM_ORDER_GBP = 100;
 export const HIGH_LEVEL_ALERT_MAILS = ['ibrahim@agiletech.ng', 'taofeekibrahimope@gmail.com']
 export const GBPsign = '£'
 export const DELIVERY_FEE_RATE_GBP: Record<string, number> = {
-    lagos: 0.5, // 7,
-    abuja: 0.5, // 7.5,
-    portharcourt: 0.5 // 8
+    lagos: 7,
+    abuja: 7.5,
+    portharcourt: 8
 }
 
 export const BYCRYPT_SALT_ROUNDS=5
