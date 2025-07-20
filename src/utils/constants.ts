d32c3ab33a58d6a9517c108c570cc632ab76612e
Ibrahim Taofeek <taofeekibrahimope@gmail.com>
2025-07-20 09:46:20 +0000

    changed min order gbp for testing

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const HANDLING_FEE_GBP = 25
-export const MINIMUM_ORDER_GBP = 100;
+export const MINIMUM_ORDER_GBP = 1 // 100;
 export const HIGH_LEVEL_ALERT_MAILS = ['ibrahim@agiletech.ng', 'taofeekibrahimope@gmail.com']
 export const GBPsign = '£'
 
