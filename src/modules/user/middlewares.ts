d3c7785d8078210a79c76140622d0ef2e32d1982
Ibrahim Taofeek <taofeekibrahimope@gmail.com>
2025-07-21 14:38:17 +0000

    made address optional in db

--- src/modules/user/middlewares.ts
+++ src/modules/user/middlewares.ts
@@ -16,7 +16,6 @@ export const registerMiddleware = (req: Request, res: Response, next: NextFuncti
                 .messages({
                     'string.pattern.base': 'Phone must contain only 11 digits',
                 }),
-            address: Joi.string().min(3).required(),
             password: Joi.string().min(6).required(),
             role: Joi.string().valid('admin', 'customer').required(),
             confirmPassword: Joi.string().valid(Joi.ref('password')).required().messages({
