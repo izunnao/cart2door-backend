ad08a9f5ef5f252b458aff89eb4e398509042273
Ibrahim Taofeek <taofeekibrahimope@gmail.com>
2025-07-24 08:49:05 +0000

    feat: added job table to prisma

---
+++ src/database/prisma/migrations/20250724084838_add_job_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Job" (
+    "id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "payload" JSONB NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Job_pkey" PRIMARY KEY ("id")
+);
